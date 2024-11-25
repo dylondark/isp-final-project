9e9ae92666d3708bf48bfb908ecd9d98b62d3b9f
lgegick <lg113@uakron.edu>
2024-11-24 21:37:44 -0500

    Fix button logic and adjust background

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,8 @@ export default function Home() {
   const router = useRouter();
 
   const handleButtonClick = () => {
-    router.push("/another-page"); // Replace with your target page path
+    console.log("Button clicked");
+    router.push("/index_page");
   };
 
   useEffect(() => {
@@ -15,7 +16,6 @@ export default function Home() {
       '/images/FBI_Image_1.jpg',
       '/images/FBI_Image_2.jpg',
       '/images/FBI_Image_3.jpg',
-      '/images/FBI_Image_4.jpg',
     ];
 
     images.forEach((image) => {
@@ -25,22 +25,38 @@ export default function Home() {
   }, []);
 
   return (
-    <div className="relative flex flex-col items-center justify-center min-h-screen p-8 background-rotation">
-      {/* Overlay to create a film effect */}
-      <div className="background-overlay"></div>
+    <div className="relative flex flex-col items-center justify-center min-h-screen p-8">
+      {/* Background Images */}
+      <div className="background-images">
+        <div className="background-image-3"></div>
+      </div>
 
-      {/* Add text at the top */}
+      {/* Overlay */}
+      <div className="background-overlay relative">
+        <div
+          className="absolute inset-0"
+          style={{
+            background:
+              'radial-gradient(ellipse at center, rgba(0,0,0,0) 40%, rgba(0,0,0,0.5) 100%)',
+            zIndex: 1,
+          }}
+        ></div>
+      </div>
+
+      {/* Content */}
       <h1 className="massive-text mb-16 w-full text-center z-10">
         FBI Most Wanted
       </h1>
 
-      {/* Add a button to navigate to another page */}
-      <button
-        className="px-6 py-3 bg-gradient-to-r from-blue-500 to-red-500 text-white font-semibold rounded-lg shadow-md hover:from-blue-600 hover:to-purple-600 hover:shadow-lg transition duration-300 ease-in-out transform hover:-translate-y-1 z-10"
-        onClick={handleButtonClick}
-      >
-        Index Page
-      </button>
+      {/* Button Container */}
+      <div className="relative z-10">
+        <button
+          className="px-6 py-3 bg-gradient-to-r from-blue-500 to-red-500 text-white font-semibold rounded-lg shadow-md hover:bg-purple-600 hover:shadow-lg transition duration-300 ease-in-out transform hover:-translate-y-1"
+          onClick={handleButtonClick}
+        >
+          Index Page
+        </button>
+      </div>
     </div>
   );
 }
